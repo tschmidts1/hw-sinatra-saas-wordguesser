ae1c83c3886c01e837e23236f54c14d691893099
Tessa Schmidts <tessa-schmidts@uiowa.edu>
2021-09-22 18:51:58 -0500

    added code to /lose and /win to hopefully not allow users to automatically "win" or "lose" by redirecting the url

--- app.rb
+++ app.rb
@@ -65,12 +65,20 @@ class WordGuesserApp < Sinatra::Base
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win
+    else
+      erb :show
+    end
+
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose
+    else
+      erb :show
+    end
   end
   
 end
